fc0d37595478edb8f1642f159665ca6b4156aa1a
hasanttonmoy <tonmoyhassan.th@gmail.com>
2023-04-17 08:17:12 -0600

    done

--- libds/setup.py
+++ libds/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='libds',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     description='A package that contains multiple data structures',
     author='Touhidul-Afif',
