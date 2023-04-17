648c196f3206cb454ff03fcb25fe3468e090b1de
hasanttonmoy <tonmoyhassan.th@gmail.com>
2023-04-17 06:41:02 -0600

    updated

--- mylib/setup.py
+++ mylib/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pagla-chacha',
-    version='0.2',
+    name='libds',
+    version='0.1',
     packages=find_packages(),
     description='A package that contains multiple data structures',
     author='Touhidul-Afif',
